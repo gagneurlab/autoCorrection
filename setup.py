c697d29b01ad91dd6e12f90d5c1b147a5de719fc
Christian Mertes <mertes@in.tum.de>
2018-04-13 20:09:25 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='autoCorrection',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT license',
     description='...',
     long_description='%s\n%s' % (
