bf4295c7b9caeafc77f1aa05917bd73b085e1d92
Christian Mertes <mertes@in.tum.de>
2018-04-13 19:52:30 +0200

    add seed option for reproducable results

--- src/autoCorrection/autoencoder.py
+++ src/autoCorrection/autoencoder.py
@@ -4,13 +4,20 @@ from .losses import NB
 from .layers import ConstantDispersionLayer
 from keras.optimizers import *
 from keras import losses
-
+import os
+from keras import backend as K
 
 
 class Autoencoder():
     def __init__(self, encoding_dim=2, size=10000, optimizer=Adam(lr=0.001),
                  choose_autoencoder=False, choose_encoder=False,
-                 choose_decoder=False, epochs=1100, batch_size=None):
+                 choose_decoder=False, epochs=1100, batch_size=None, seed = None):
+        if seed is not None:
+            tf.set_random_seed(seed)
+            os.environ['PYTHONHASHSEED'] = seed
+            session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+            sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+            K.set_session(sess)
         self.encoding_dim = encoding_dim
         self.size = size
         self.epochs = epochs
